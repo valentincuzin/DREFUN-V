e5d864a4f3ec35d26cb3c94583df2d47236573f3
Alexandre Faure <alexandre.louis.faure69@gmail.com>
2024-12-09 14:31:22 +0100

    ajout des "objectif_metrics" pour comparer les performance des politiques sur d'autres critères que le reward

--- DREFUN.py
+++ DREFUN.py
@@ -1,9 +1,10 @@
+from logging import getLogger
+from typing import Callable, Dict, List
+
 import gymnasium as gym
 import numpy as np
-from typing import List, Dict, Callable
 
 from OllamaChat import OllamaChat
-from logging import getLogger
 
 logger = getLogger("DREFUN")
 
@@ -203,6 +204,18 @@ class DREFUN:
         raw_states, raw_rewards, raw_sr_test = self.test_policy(raw_policy)
         states, rewards, sr_test = self.test_policy(policy, self.reward_functions[-1])
         # TODO penser aux métrics objective propre à l'environnment, une raison de faire une class environnement extend de celle de base ?
+        perso_raw_states = []
+        perso_states = []
+        
+        for objective_metric in objectives_metrics:
+            perso_raw_states = objective_metric(raw_states)
+            perso_states = objective_metric(states)
+
+        for i in range(len(perso_raw_states)):
+            logger.info(
+                f"{perso_raw_states[i][0]} : human {perso_raw_states[i][1]} llm {perso_states[i][1]}"
+            )
+        
         logger.info(
             "the policy with human reward:"
             + f"\n- during the train: SR {raw_sr}, nb_ep {raw_nb_ep}"
--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from log.log_config import init_logger
 from logging import getLogger
-from DREFUN import DREFUN
-from RLalgo import PolitiqueDirectSearch
+
 import gymnasium as gym
+
+from DREFUN import DREFUN
+from log.log_config import init_logger
+from RLalgo import PolitiqueDirectSearch
+
+
+def objective_metric_CartPole(states):
+    """
+    Objective metric for the CartPole environment.
+    Calculates a score for the given state on a particular observation of the CartPole environment.
+    
+    :param state: The state of the CartPole environment.
+    :return: a table of tuples containing the string name of the metric and the value of the metric.
+    """
+    
+    # Calculate the difference between the pole angle and the median of the pole angle range
+    pole_angle_diff = 0
+    for state in states:
+        pole_angle = state[2]
+        pole_angle_diff += abs(pole_angle)
+    pole_angle_diff = pole_angle_diff / len(states)
+    
+    # Calculate the difference between the pole position and the median of the pole position range
+    pole_position_diff = 0
+    for state in states:
+       pole_position = state[0]
+       pole_position_diff += abs(pole_position)
+    pole_position_diff = pole_position_diff / len(states)
+    
+    result = [("pole_angle_diff", pole_angle_diff), ("pole_position_diff", pole_position_diff)]
+    
+    return result
+    
+
+
+
+
+
 
 if __name__ == "__main__":  # TODO parametrer le mode debug
     init_logger()
     logger = getLogger("DREFUN")
-    env = gym.make("LunarLander-v3")
+    env = gym.make("CartPole-v1")
     learning_method = PolitiqueDirectSearch(env)
     drefun = DREFUN(learning_method, env)
 
@@ -21,5 +57,10 @@ if __name__ == "__main__":  # TODO parametrer le mode debug
         Since the goal is to keep the pole upright for as long as possible, by default, a reward of +1 is given for every step taken, including the termination step. The default reward threshold is 500 for v1
         """,
     )
+    
+    objective_metric = [objective_metric_CartPole]
 
-    drefun.evaluate_policy()
+    drefun.evaluate_policy(objectives_metrics=objective_metric)
+
+
+
